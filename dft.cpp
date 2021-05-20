b5dbceb9dd8bc3285c84fcba8bd5137bb3e5c696
Alper Balmumcu <71339227+abalmumcu@users.noreply.github.com>
2021-05-20 22:40:57 +0300

    Update dft.cpp

--- dft.cpp
+++ dft.cpp
@@ -14,25 +14,25 @@ using namespace std;
 
 
 int main() {
-    vector <vector <double>> given_array = {    // Verilen görüntü matrisi
+    vector <vector <double>> given_array = {    
         {3,2,1},
         {6,5,4},
         {9,8,7}};
     
-    double output_array[3][3];      // çıktı olarak kullanılacak görüntü
-    int width=3;    // görüntünün genişlik ve yükseklik değerleri
+    double output_array[3][3];      
+    int width=3;    
     int height=3;
 
-    complex <double> **dft_array = new complex <double>*[width];    // complex olarak işlem yapılacak dft_array tanımlandı.
+    complex <double> **dft_array = new complex <double>*[width];   
     for (int b=0;b<width;b++){                      
-        dft_array[b]= new complex <double>[height];     // dft_array değişkeni [3][3] olarak tanımlandı.
+        dft_array[b]= new complex <double>[height];    
     }
 
-    complex<double> sum=0;  // Satırların fourieri alındığında kullanılacak temp.
-    complex<double> sum2=0; // Sütunların fourieri alındığında kullanılacak temp.
-    double pi = 3.141592653589793;  // Pi sayısı tanımlandı
+    complex<double> sum=0; 
+    complex<double> sum2=0; 
+    double pi = 3.141592653589793;
 
-    for (int i=0; i<3; i++){    // Buradaki for düngüsü ile given_array matrisi, complex dft_array değişkenine atandı.
+    for (int i=0; i<3; i++){    
         for (int j=0; j<3; j++){
             complex<double> cplx(given_array[i][j], 0);
             dft_array[i][j]=cplx;   
@@ -40,7 +40,7 @@ int main() {
     }
     cout << "\nOut of the 1D DFT of Row\n";
 
-    for (int x=0; x<3; x++){    // Bu bölümde Satırların fourier dönüşümü yapıldı.
+    for (int x=0; x<3; x++){    
         sum=0;
         for (int y=0; y<3; y++){
             sum+=(dft_array[x][y].real())*cos((2*pi*x*y)/3)-(dft_array[x][y].imag())*sin((2*pi*x*y)/3);
@@ -51,7 +51,7 @@ int main() {
     
     cout << "\n\nOut of the 1D DFT of Column\n";
     
-    for (int x=0; x<3; x++){    // Bu bölümde yukarıda Fourier dönüşümü yapılan Satırların, Sütünlar için fourier dönüşümü yapıldı.
+    for (int x=0; x<3; x++){   
         sum2=0;
         for (int y=0; y<3; y++){
             sum2+=(dft_array[y][x].real())*cos((2*pi*x*y)/3)-(dft_array[y][x].imag())*sin((2*pi*x*y)/3);
@@ -62,13 +62,13 @@ int main() {
     
     cout << "\n\nResults:\n";
 
-    for (int i=0; i<3; i++){    // En son olarak ayrı ayrı satır ve sütun için 1D DFT işleminden sonra değerlerin reel kısımları output_arraya atandı.
+    for (int i=0; i<3; i++){   
         for (int j=0; j<3; j++){
         output_array[i][j]=dft_array[i][j].real();
         }
     }    
    
-    for ( int i = 0; i < 3; i++ ){  // Sonuç değerlerini yazdırma kısmı
+    for ( int i = 0; i < 3; i++ ){ 
         for ( int j = 0; j < 3; j++ ) {      
         cout << "output_array[" << i << "][" << j << "]: ";
         cout << output_array[i][j]<< endl;
